b2c68994d07d850ac9b2caa58e12b69ee58acf7a
Wallace Carvalho <wallace.carvalho789@gmail.com>
2021-01-29 01:32:18 -0300

    Command 'help' Improoved

--- commands/info/help.js
+++ commands/info/help.js
@@ -6,13 +6,33 @@ module.exports = {
     const Discord = require("discord.js");
     const { comandos } = require("../../config.json");
 
+    let prefix = "!";
+    let desc = [];
     let msgID;
     let embed = new Discord.MessageEmbed()
       .setDescription("")
       .setTimestamp()
       .setColor("1fd14f")
-      .setTitle("LOG:");
+      .setTitle("COMANDOS:");
 
     msgID = await message.channel.send(embed);
+
+    comandos.forEach(async (comando) => {
+      let descricao = "\n";
+      descricao += prefix;
+      descricao += comando.name;
+      descricao += "\n";
+      descricao += comando.desc;
+      await desc.push(descricao);
+
+      let embed = new Discord.MessageEmbed()
+        .setDescription(desc)
+        .setTimestamp()
+        .setColor("1fd14f")
+        .setTitle("COMANDOS:");
+
+      msgID.edit(embed);
+      console.log(descricao);
+    });
   },
 };
